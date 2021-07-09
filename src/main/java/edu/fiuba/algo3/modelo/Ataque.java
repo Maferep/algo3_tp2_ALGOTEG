cb3c128cb8ba6690f46ec70a13e0b1a10ad55bc9
mpiligaddi <mgaddi@fi.uba.ar>
2021-07-09 20:12:21 -0300

    Se crea la clase Ataque y se delegan las responsabilidades de ataque a la misma, desde Pais.

---
+++ src/main/java/edu/fiuba/algo3/modelo/Ataque.java
@@ -0,0 +1,36 @@
+package edu.fiuba.algo3.modelo;
+public class Ataque {
+
+public long calcularCantidadVictorias(TiroDeDados dadosAtacante, TiroDeDados dadosDefensor) {
+        return dadosAtacante
+        .batallarConDesventaja(dadosDefensor)
+        .stream()
+        .filter(
+        ganador -> ganador == dadosAtacante
+        ).count();
+}
+
+public long calcularCantidadDerrotas(TiroDeDados dadosAtacante, TiroDeDados dadosDefensor) {
+        return dadosAtacante
+        .batallarConDesventaja(dadosDefensor)
+        .stream()
+        .filter(
+        ganador -> ganador == dadosDefensor
+        ).count();
+}
+public Boolean atacar(Pais atacante, Pais defensor, int cantEjercitos) {
+        //tirar dados
+        TiroDeDados dadosAtacante = new TiroDeDados(Math.min(cantEjercitos, 3-1)); // siempre es la cantidad de ejercitos menos 1
+        TiroDeDados dadosDefensor = new TiroDeDados(Math.min(defensor.ejercitos, 3));
+        //calcular victorias de nuestros dados
+        long cantVictorias = calcularCantidadVictorias(dadosAtacante, dadosDefensor);
+        //calcular derrotas de nuestros dados
+        long cantDerrotas = calcularCantidadDerrotas(dadosAtacante, dadosDefensor);
+        //cuento la cantidad de derrotas para poder saber cuantos soldados quitar.
+        atacante.quitarEjercitos(cantDerrotas);
+        defensor.quitarEjercitos(cantVictorias);
+        //es victorioso si derrota tantos dados
+        //como ejercitos tiene el pais defensor
+        return cantVictorias >= defensor.ejercitos;
+    }
+}
--- src/main/java/edu/fiuba/algo3/modelo/Pais.java
+++ src/main/java/edu/fiuba/algo3/modelo/Pais.java
@@ -22,39 +22,12 @@ public class Pais {
         ejercitos += cantidadEjercitos;
 	}
 
-	public void quitarEjercitos(long cantidadEjercitos) { ejercitos -= cantidadEjercitos; }
-
-	public long calcularCantidadVictorias(TiroDeDados dadosAtacante, TiroDeDados dadosDefensor) {
-		return dadosAtacante
-				.batallarConDesventaja(dadosDefensor)
-				.stream()
-				.filter(
-						ganador -> ganador == dadosAtacante
-				).count();
+	public void quitarEjercitos(long cantidadEjercitos) {
+		ejercitos -= cantidadEjercitos;
 	}
 
-	public long calcularCantidadDerrotas(TiroDeDados dadosAtacante, TiroDeDados dadosDefensor) {
-		return dadosAtacante
-				.batallarConDesventaja(dadosDefensor)
-				.stream()
-				.filter(
-						ganador -> ganador == dadosDefensor
-				).count();
-	}
 	public Boolean atacar(Pais defensor, int cantEjercitos) {
-		//tirar dados
-		TiroDeDados dadosAtacante = new TiroDeDados(Math.min(cantEjercitos, 3-1));// siempre es la cantidad de ejercitos menos 1
-		TiroDeDados dadosDefensor = new TiroDeDados(Math.min(defensor.ejercitos, 3));
-		//calcular victorias de nuestros dados
-		long cantVictorias = calcularCantidadVictorias(dadosAtacante, dadosDefensor);
-		//calcular derrotas de nuestros dados
-		long cantDerrotas = calcularCantidadDerrotas(dadosAtacante, dadosDefensor);
-		//cuento la cantidad de derrotas para poder saber cuantos soldados quitar.
-		quitarEjercitos(cantDerrotas);
-		defensor.quitarEjercitos(cantVictorias);
-		//es victorioso si derrota tantos dados 
-		//como ejercitos tiene el pais defensor
-		return cantVictorias >= defensor.ejercitos;
+		Ataque nuevoAtaque = new Ataque();
+		return nuevoAtaque.atacar(this, defensor, cantEjercitos);
 	}
-
 }
