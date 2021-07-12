ec9df34f1ef39c3ead83ae414c02cbf291921b2b
Mafer <mf.pont.tv@gmail.com>
2021-07-12 11:27:48 -0300

    implementa dado falso

--- src/main/java/edu/fiuba/algo3/modelo/Ataque.java
+++ src/main/java/edu/fiuba/algo3/modelo/Ataque.java
@@ -4,6 +4,8 @@ import edu.fiuba.algo3.modelo.Interfaces.*;
 public class Ataque {
         ITiroDeDados dadosAtacante;
         ITiroDeDados dadosDefensor;
+
+        //TODO: deprecate
         public Ataque(Pais defensor, int cantEjercitos, int numeroAtacante, int numeroDefensor) {
                 dadosAtacante = new TiroDeDados(Math.min(cantEjercitos, 3),numeroAtacante);
                 dadosDefensor = new TiroDeDados(Math.min(defensor.ejercitos, 3),numeroDefensor);
@@ -14,6 +16,8 @@ public class Ataque {
         }
 
         public Ataque(Pais defensor, ITiroDeDados dadoFalso) {
+                dadosAtacante = dadoFalso;
+                dadosDefensor = dadoFalso;
         }
         public long calcularCantidadVictorias(ITiroDeDados dadosAtacante, ITiroDeDados dadosDefensor) {
                 return dadosAtacante.cantidadVictorias();
--- src/main/test/edu/fiuba/algo3/modelo/DadoFalso.java
+++ src/main/test/edu/fiuba/algo3/modelo/DadoFalso.java
@@ -1,12 +1,30 @@
 package edu.fiuba.algo3.modelo;
 
 import edu.fiuba.algo3.modelo.Interfaces.ITiroDeDados;
-import java.util.List; 
+
 public class DadoFalso implements ITiroDeDados {
+    int cantidadVictorias;
+    int cantidadDerrotas;
+
     public DadoFalso(int cantidadVictorias, int cantidadDerrotas) {
 	}
-	public int cantidadDados() {return 3;}
-    public int obtenerDado(int i) {return 3;}
+	public int cantidadDados() {
+        return cantidadVictorias + cantidadDerrotas;
+    }
+
+    public int obtenerDado(int i) {
+        return 0;
+    }
     public void ordenarDescendientemente() {}
-    public List<ITiroDeDados> batallarConDesventaja(ITiroDeDados rival) {return null;}
+    public void batallarConDesventaja(ITiroDeDados rival) {}
+
+    @Override
+    public int cantidadVictorias() {
+        return this.cantidadVictorias;
+    }
+
+    @Override
+    public int cantidadDerrotas() {
+        return this.cantidadDerrotas;
+    }
 }
