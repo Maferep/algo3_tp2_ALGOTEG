a4c771f2f177c5b8dc2fdb6be2b560747422ef7f
Mafer <mf.pont.tv@gmail.com>
2021-07-13 14:37:55 -0300

    Añade UID a excepcion que causaba quejas

--- src/main/java/edu/fiuba/algo3/modelo/excepciones/FichasInsuficientesError.java
+++ src/main/java/edu/fiuba/algo3/modelo/excepciones/FichasInsuficientesError.java
@@ -1,6 +1,9 @@
 package edu.fiuba.algo3.modelo.excepciones;
 
 public class FichasInsuficientesError extends Exception {
+
+    private static final long serialVersionUID = -8618114065857941861L;
+
     public FichasInsuficientesError(String msg) {
         super(msg);
     }
