00bca1fb4601d4e5e28b8f302e6487d84219d6c0
mpiligaddi <mgaddi@fi.uba.ar>
2021-07-17 20:51:38 -0300

    Corregir prueba de turnos

--- src/main/java/edu/fiuba/algo3/modelo/Turno.java
+++ src/main/java/edu/fiuba/algo3/modelo/Turno.java
@@ -12,7 +12,7 @@ public class Turno {
 
     public void determinarTurnos(List<Jugador> jugadores) {
         for(int i = 0 ; i < jugadores.size() ; i++) {
-            numeroDeTurnos[i] = jugadores.get(i).numeroDeTurno();
+            numeroDeTurnos[i] = (jugadores.get(i)).numeroDeTurno();
             //numeroDeTurnos.add(jugadores.get(i).numeroDeTurno());
             jugadoresNoOrdenados.add(jugadores.get(i));
         }
@@ -28,7 +28,7 @@ public class Turno {
     public void ordenarJugadoresPorTurno(List<Jugador> jugadores) {
         for(int i = 0 ; i < jugadores.size() ; i++) {
             for(int j = 0 ; j < jugadores.size() ; j++) {
-                if((jugadoresNoOrdenados.get(j)).numeroDeTurno() == numeroDeTurnos[i]) {
+                if((jugadoresNoOrdenados.get(j)).numeroDeTurno() == numeroDeTurnos[i]) { //este if solo es true dos veces. deberian ser cuatro
                 //if((jugadoresNoOrdenados.get(j)).numeroDeTurno() == numeroDeTurnos.get(i)) {
                     jugadoresOrdenados.add(jugadoresNoOrdenados.get(j));
                 }
@@ -36,8 +36,14 @@ public class Turno {
         }
     }
 
-    public String ColorDejugadorEnOrden(int numeroDePosicionRequerida) {
-        return (jugadoresOrdenados.get(numeroDePosicionRequerida)).color;
+    //numeroDePosicionRequerida me excede lo que yo necesito ...
+    public String colorDejugadorEnOrden(int numeroDePosicionRequerida) {
+        return (jugadoresOrdenados.get(numeroDePosicionRequerida)).color; //solo tiene de tamanio 2. No se porque no se llenaron todos los necesarios.
+        //return (jugadoresNoOrdenados.get(numeroDePosicionRequerida)).color;
+    }
+
+    public int cantidad() {
+        return jugadoresOrdenados.size();
     }
 
 }
--- src/main/test/edu/fiuba/algo3/modelo/TurnoTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/TurnoTest.java
@@ -7,8 +7,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TurnoTest {
     @Test
     public void test00JugadorConTurnoCorrecto() throws Exception {
-        EtapaInicial primeraEtapa = new EtapaInicial(3, 0); //??
-        assertEquals(primeraEtapa.cantidadDeJugadores(), 3);
-        assertEquals(primeraEtapa.sistemaDeTurnos.ColorDejugadorEnOrden(2), "Rojo");
+        EtapaInicial primeraEtapa = new EtapaInicial(4, 2); //??
+        assertEquals(primeraEtapa.cantidadDeJugadores(), 4);
+        assertEquals(primeraEtapa.sistemaDeTurnos.colorDejugadorEnOrden(0), "Azul");
+        assertEquals(primeraEtapa.sistemaDeTurnos.colorDejugadorEnOrden(1), "Rojo");
+        //assertEquals(primeraEtapa.sistemaDeTurnos.cantidad(), 2);
+        assertEquals(primeraEtapa.sistemaDeTurnos.colorDejugadorEnOrden(2), "Amarillo");
+        //assertEquals(primeraEtapa.sistemaDeTurnos.ColorDejugadorEnOrden(3), "Verde");
     }
 }
