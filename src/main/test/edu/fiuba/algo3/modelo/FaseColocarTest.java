db871357f422f1a32ed10c4873862b6dba988ae5
mpiligaddi <mgaddi@fi.uba.ar>
2021-07-21 20:41:03 -0300

    Nuevas pruebas para la etapa de colocar

--- src/main/test/edu/fiuba/algo3/modelo/FaseColocarTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/FaseColocarTest.java
@@ -4,6 +4,8 @@ import edu.fiuba.algo3.modelo.Interfaces.IFase;
 import edu.fiuba.algo3.modelo.Interfaces.ITurno;
 import edu.fiuba.algo3.modelo.Mocks.JugadorFactoryMock;
 import edu.fiuba.algo3.modelo.Mocks.TurnoMockUnJugador;
+import edu.fiuba.algo3.modelo.excepciones.FichasInsuficientesError;
+import edu.fiuba.algo3.modelo.excepciones.PaisNoExistenteError;
 import edu.fiuba.algo3.modelo.factories.JuegoFactory;
 import edu.fiuba.algo3.modelo.fases.FaseInicio;
 import edu.fiuba.algo3.modelo.fases.FaseColocar;
@@ -40,18 +42,57 @@ public class FaseColocarTest {
             .stream()
             .map(n -> new Pais(n))
             .collect(Collectors.toList());
+
     //Juego de una ronda con 2 jugadores.
     //En esta ronda no se deben atacar pero sí colocar nuevos ejércitos.
     @Test
     public void test00RondaDeDosJugadoresSinAtaques() throws Exception {
-        // fase inicial
         ITurno unJugador = new TurnoMockUnJugador(paisesJugadorUno);
-        new FaseInicio(unJugador);
         FaseColocar fase = new FaseColocar(unJugador,paisesJugadorUno);
         Pais pais = new Pais("Estados Unidos");
-        // fase juego : colocar
         fase.ubicarEjercitosEnPais(3, pais);
         assertTrue(fase.faseCompletada());
         assertEquals(pais.cantidadEjercitos(), 3);
+
+        ITurno unSegundoJugador = new TurnoMockUnJugador(paisesJugadorDos);
+        FaseColocar faseNueva = new FaseColocar(unSegundoJugador,paisesJugadorDos);
+        Pais paisNuevo = new Pais("Bolivia");
+        faseNueva.ubicarEjercitosEnPais(2, paisNuevo);
+        assertTrue(faseNueva.faseCompletada());
+        assertEquals(paisNuevo.cantidadEjercitos(), 2);
+    }
+
+    @Test
+    public void test01RondaDeDosJugadoresNoHaySuficientesFichasParaAgregarAUnPais() throws Exception {
+        ITurno unJugador = new TurnoMockUnJugador(paisesJugadorUno);
+        FaseColocar fase = new FaseColocar(unJugador,paisesJugadorUno);
+        Pais pais = new Pais("Estados Unidos");
+        fase.ubicarEjercitosEnPais(3, pais);
+        assertTrue(fase.faseCompletada());
+        assertEquals(pais.cantidadEjercitos(), 3);
+
+        ITurno unSegundoJugador = new TurnoMockUnJugador(paisesJugadorDos);
+        FaseColocar faseNueva = new FaseColocar(unSegundoJugador,paisesJugadorDos);
+        Pais paisNuevo = new Pais("Bolivia");
+        assertThrows(FichasInsuficientesError.class, () -> {
+            faseNueva.ubicarEjercitosEnPais(1000, paisNuevo);
+        } );
+    }
+
+    @Test
+    public void test03NoExistePaisError() throws Exception {
+        ITurno unJugador = new TurnoMockUnJugador(paisesJugadorUno);
+        FaseColocar fase = new FaseColocar(unJugador,paisesJugadorUno);
+        Pais pais = new Pais("Estados Unidos");
+        fase.ubicarEjercitosEnPais(3, pais);
+        assertTrue(fase.faseCompletada());
+        assertEquals(pais.cantidadEjercitos(), 3);
+
+        ITurno unSegundoJugador = new TurnoMockUnJugador(paisesJugadorDos);
+        FaseColocar faseNueva = new FaseColocar(unSegundoJugador,paisesJugadorDos);
+        Pais paisNuevo = new Pais("Belgica");
+        assertThrows(PaisNoExistenteError.class, () -> {
+            faseNueva.ubicarEjercitosEnPais(1, paisNuevo);
+        } );
     }
 }
