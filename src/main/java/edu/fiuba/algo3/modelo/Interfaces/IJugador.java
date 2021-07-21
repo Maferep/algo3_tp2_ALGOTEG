0d3772664436de5d1ceff005c2e16c70328ebead
Mafer <mf.pont.tv@gmail.com>
2021-07-21 17:19:54 -0300

    quita implementación innecesaria de interfaz

--- src/main/java/edu/fiuba/algo3/modelo/Interfaces/IJugador.java
+++ src/main/java/edu/fiuba/algo3/modelo/Interfaces/IJugador.java
@@ -1,25 +1,13 @@
 package edu.fiuba.algo3.modelo.Interfaces;
 
-import edu.fiuba.algo3.modelo.Jugador;
-import edu.fiuba.algo3.modelo.Pais;
+import edu.fiuba.algo3.modelo.*;
 import edu.fiuba.algo3.modelo.excepciones.EjercitosException;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
-
-import edu.fiuba.algo3.modelo.*;
-import edu.fiuba.algo3.modelo.excepciones.*;
 
 public interface IJugador {
 
     List<Jugador> construirJugadores(List<String> colores, int cantidad) throws EjercitosException;
-
-    private List<Jugador> jugadoresDeColores(List<String> colores) {
-        return colores.stream()
-                .map(c -> new Jugador(c))
-                .collect(Collectors.toList());
-    }
 
     void asignarEjercitosAJugadores(List<Jugador> jugadores) throws EjercitosException;
 
