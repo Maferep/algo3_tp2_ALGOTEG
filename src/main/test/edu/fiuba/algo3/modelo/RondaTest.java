db3a8833de6807b29cf9cc88a3e4da7227c071ab
Mafer <mf.pont.tv@gmail.com>
2021-07-21 17:08:22 -0300

    Unir EtapasTest y RondaTest a un solo archivo

--- src/main/test/edu/fiuba/algo3/modelo/RondaTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/RondaTest.java
@@ -1,17 +1,54 @@
 package edu.fiuba.algo3.modelo;
 
 import edu.fiuba.algo3.modelo.Interfaces.IFase;
-import edu.fiuba.algo3.modelo.factories.JuegoFactory;
-import edu.fiuba.algo3.modelo.factories.JugadorFactoryMock;
+import edu.fiuba.algo3.modelo.factories.*;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class RondaTest {
+    
     JuegoFactory juegoBuilder = new JuegoFactory();
-    //Juego de una ronda con 2 jugadores. En esta ronda no se deben atacar pero sí colocar nuevos ejércitos.
+    JugadorFactory tipoDeJugador = new JugadorFactory();
     @Test
-    public void test00RondaDeDosJugadoresSinAtaques() throws Exception {
+    public void test00AgregarJugadores() throws Exception {
+        juegoBuilder.crearJuegoTEG(3, tipoDeJugador);
+    }
+
+    @Test
+    public void test01agregarEjercitos() throws Exception {
+        // internamente, asigna paises y objetivos a 4 colores
+        IFase fase = juegoBuilder.crearJuegoTEG(4, tipoDeJugador);
+        assertFalse(fase.faseCompletada());
+        fase.asFaseInicio().ubicarEjercitosEnPais(3, new Pais("EEUU"));
+        fase.asFaseInicio().ubicarEjercitosEnPais(5, new Pais("EEUU"));
+        assertTrue(fase.faseCompletada());
+    }
+
+    @Test
+    public void test02PasarPorEtapasDistintas() throws Exception {
+        //genera una etapa de inicio en estado 'finalizado' de ejemplo
+        IFase fase = juegoBuilder.crearJuegoTEG(4, tipoDeJugador);
+        assertFalse(fase.faseCompletada());
+
+        Pais pais = new Pais("Francia");
+        pais.agregarEjercitos(3);
+        Pais pais2 = new Pais("Alemania");
+        pais2.agregarEjercitos(3);
+
+        fase.asFaseInicio().ubicarEjercitosEnPais(3, pais);
+        assertTrue(fase.faseCompletada());
+        fase = fase.siguienteFase();
+
+        fase.asFaseAtacar().atacar(pais, 1, pais2);
+        assertTrue(fase.faseCompletada());
+        fase = fase.siguienteFase();
+    }
+    
+    //Juego de una ronda con 2 jugadores. 
+    //En esta ronda no se deben atacar pero sí colocar nuevos ejércitos.
+    @Test
+    public void test03RondaDeDosJugadoresSinAtaques() throws Exception {
         // fase inicial
         JugadorFactoryMock tipoDeJugador = new JugadorFactoryMock();
         IFase fase = juegoBuilder.crearJuegoTEG(2, tipoDeJugador);
