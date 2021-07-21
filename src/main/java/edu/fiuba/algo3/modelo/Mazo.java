48feefb07def89773ba0ab1ce4d4bb3a4a4dc172
Mafer <mf.pont.tv@gmail.com>
2021-07-21 14:48:42 -0300

    mazo es publico

--- src/main/java/edu/fiuba/algo3/modelo/Mazo.java
+++ src/main/java/edu/fiuba/algo3/modelo/Mazo.java
@@ -3,16 +3,16 @@ package edu.fiuba.algo3.modelo;
 import java.util.*;
 import java.util.stream.*;
 
-class Mazo {
+public class Mazo {
     LinkedList<Tarjeta> tarjetas = new LinkedList<Tarjeta>();
-    Mazo(List<Pais> paises) {
+    public Mazo(List<Pais> paises) {
         tarjetas.addAll(
             paises.stream()
                 .map(p -> new Tarjeta(p))
                 .collect(Collectors.toList())
         );
     }
-    Tarjeta obtenerTarjeta() {
+    public Tarjeta obtenerTarjeta() {
         return tarjetas.getFirst();
     }
 }
