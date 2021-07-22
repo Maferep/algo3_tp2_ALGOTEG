1860bca7e13dbab43dc0b6828537ba8aff80c836
Chiara Di Nardo <cdinardoex@bice.cl>
2021-07-22 00:51:41 -0300

    Test de Continente pasan

--- src/main/java/edu/fiuba/algo3/modelo/Continente.java
+++ src/main/java/edu/fiuba/algo3/modelo/Continente.java
@@ -11,7 +11,7 @@ public class Continente {
 
     public Boolean fueConquistadoPor(Jugador jugador) {
         for (int i = 0; i < this.paises.size(); i++) {
-            if (this.paises.get(i).obtenerConquistador().obtenerColor() == jugador.obtenerColor()) {
+            if (this.paises.get(i).obtenerConquistador().obtenerColor() != jugador.obtenerColor()) {
                 return false;
             }
         } return true;
---
+++ src/main/test/edu/fiuba/algo3/modelo/ContinenteTest.java
@@ -0,0 +1,42 @@
+package edu.fiuba.algo3.modelo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ContinenteTest {
+    public List<Pais> paises = Arrays.asList(
+            "Estados Unidos",
+            "Canadá",
+            "México",
+            "Groenlandia")
+            .stream()
+            .map(n -> new Pais(n))
+            .collect(Collectors.toList());
+
+    @Test
+    public void test00JugadorConqusitaContinente() {
+        Jugador jugador = new Jugador("Azul");
+        Continente continente = new Continente(paises);
+
+        paises.forEach(pais -> pais.asignarConquistador(jugador));
+
+        assertTrue(continente.fueConquistadoPor(jugador));
+    }
+
+    @Test
+    public void test01JugadorNoConquistaContinente() {
+        Jugador jugadorRojo = new Jugador("Rojo");
+        Jugador jugadorAzul = new Jugador("Azul");
+
+        Continente continente = new Continente(paises);
+
+        paises.forEach(pais -> pais.asignarConquistador(jugadorRojo));
+
+        assertFalse(continente.fueConquistadoPor(jugadorAzul));
+    }
+
+}
