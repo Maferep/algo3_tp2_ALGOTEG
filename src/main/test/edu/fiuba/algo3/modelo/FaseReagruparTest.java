69f527621fef9e0dbba4e891ea069e232ff7136a
Mafer <mf.pont.tv@gmail.com>
2021-07-24 16:58:09 -0300

    quita comentario superfluo

--- src/main/test/edu/fiuba/algo3/modelo/FaseReagruparTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/FaseReagruparTest.java
@@ -30,8 +30,6 @@ public class FaseReagruparTest {
             .map(n -> new Pais(n))
             .collect(Collectors.toList());
 
-    //Juego de una ronda con 2 jugadores.
-    //En esta ronda no se deben atacar pero sí colocar nuevos ejércitos.
     @Test
     public void test00Reagrupar() throws EjercitosException {
         FaseReagrupar reagrupar = new FaseReagruparSinConquista(
