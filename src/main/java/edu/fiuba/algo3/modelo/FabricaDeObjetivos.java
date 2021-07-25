b0215faf70c1cee50a3ea7746788b9f3885b7fe6
mpiligaddi <mgaddi@fi.uba.ar>
2021-07-25 19:59:26 -0300

    Refactor objetivos

--- src/main/java/edu/fiuba/algo3/modelo/FabricaDeObjetivos.java
+++ src/main/java/edu/fiuba/algo3/modelo/FabricaDeObjetivos.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-//hacer mock?
+//hacer mock? porque testeo con un determinado orden en objetivoTest
 public class FabricaDeObjetivos {
 
     List<IObjetivo> objetivos = new ArrayList<>();
