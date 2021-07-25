3647f23614bded44c987f4b8a67d23ebfd373243
Mafer <mf.pont.tv@gmail.com>
2021-07-25 11:22:14 -0300

    agrega tests a juego

--- src/main/test/edu/fiuba/algo3/modelo/JuegoTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/JuegoTest.java
@@ -16,19 +16,6 @@ import edu.fiuba.algo3.modelo.Interfaces.*;
 
 
 public class JuegoTest {
-
-    List<IPais> paises = Arrays
-            .asList(
-            "Estados Unidos",
-            "Canadá", 
-            "Brasil", 
-            "Bolivia",
-            "Colombia",
-            "Chile",
-            "Ecuador")
-            .stream()
-            .map(n -> new Pais(n))
-            .collect(Collectors.toList());
     @Test
     public void test01avanzarFasesDeJuego() throws FaseErroneaException, Exception {
         Juego juego = new Juego(3);
@@ -47,6 +34,9 @@ public class JuegoTest {
         IPais defensor = paisesDeUltimo.get(0);
         IPais atacante = paisesDeJugadorActual.get(0);
 
+        defensor.agregarAdyacente(atacante);
+        atacante.agregarAdyacente(defensor);
+
         assertNotEquals(null, defensor);
         assertNotEquals(null, atacante);
         assertNotEquals(null, juego.jugadorActual());
@@ -57,4 +47,28 @@ public class JuegoTest {
         juego.siguienteFase();
         assertEquals(1, juego.jugadorActual().cantidadTarjetas());
     }
+
+    @Test
+    public void test02TransferirEjercitos() throws FaseErroneaException, Exception {
+        Juego juego = new Juego(3);
+
+        List<IPais> paisesDeUltimo = null;
+        for(int i = 0; i < juego.cantidadDePaises(); i++) {
+            paisesDeUltimo = juego.jugadorActual().obtenerPaises();
+            juego.ubicarEjercitosEnPais(3, paisesDeUltimo.get(0));
+            juego.siguienteTurno();
+        }
+
+        List<IPais> paisesDeJugadorActual = juego.jugadorActual().obtenerPaises();
+        IPais pais1 = paisesDeJugadorActual.get(0);
+        IPais pais2 = paisesDeJugadorActual.get(1);
+        pais1.agregarAdyacente(pais2);
+        pais2.agregarAdyacente(pais1);
+        juego.siguienteFase();
+        juego.siguienteFase();
+        juego.transferirEjercitos(3, pais1, pais2);
+    }
+    
+
+    
 }
