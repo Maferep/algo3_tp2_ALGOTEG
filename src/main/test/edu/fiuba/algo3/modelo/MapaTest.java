beacc39623bacf0eb1df3bcbcf1dfba149656233
Chiara Di Nardo <cdinardo@fi.uba.ar>
2021-07-25 20:21:14 -0300

    Tests de Mapa implementados y pasados

---
+++ src/main/test/edu/fiuba/algo3/modelo/MapaTest.java
@@ -0,0 +1,37 @@
+package edu.fiuba.algo3.modelo;
+
+import edu.fiuba.algo3.modelo.Interfaces.IJugador;
+import edu.fiuba.algo3.modelo.Interfaces.IPais;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MapaTest {
+
+    @Test
+    public void test00CrearMapa() {
+        Mapa mapa = new Mapa();
+        List<IPais> paises = mapa.obtenerPaises();
+
+        Assert.assertEquals(50, paises.size());
+    }
+
+    @Test
+    public void test01MapaAsignaPaises() {
+        Mapa mapa = new Mapa();
+        List<IJugador> jugadores = Arrays.asList(
+                "Rojo",
+                "Azul")
+                .stream()
+                .map(color -> new Jugador(color))
+                .collect(Collectors.toList());
+
+        mapa.asignarPaises(jugadores);
+
+        Assert.assertFalse(jugadores.get(0).obtenerPaises().isEmpty());
+        Assert.assertFalse(jugadores.get(1).obtenerPaises().isEmpty());
+    }
+}
