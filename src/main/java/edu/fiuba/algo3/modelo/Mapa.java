bd0ba76571025ee4aa0db8a8affab079119f6fd1
Chiara Di Nardo <cdinardo@fi.uba.ar>
2021-07-31 01:25:46 -0300

    Metodo de continentes conquistados por jugador implementado en Mapa. JSON de continentes iniciado

--- src/main/java/edu/fiuba/algo3/modelo/Mapa.java
+++ src/main/java/edu/fiuba/algo3/modelo/Mapa.java
@@ -2,11 +2,13 @@ package edu.fiuba.algo3.modelo;
 
 import edu.fiuba.algo3.modelo.Interfaces.*;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Mapa implements IMapa {
     private List<IPais> paises;
+    private List<Continente> continentes;
 
     public Mapa() {
         MapaFachada mapaFachada = new MapaFachada();
@@ -35,6 +37,14 @@ public class Mapa implements IMapa {
             IPais actual = paises.get(i);
             jugadores.get(i % jugadores.size()).inicializarPais(actual);
         }
+    }
+
+    public List<Continente> continentesConquistados(Jugador jugador) {
+        List<Continente> continentesConquistados = new ArrayList<Continente>();
+        for (Continente continente : continentes) {
+            if ( continente.fueConquistadoPor(jugador) ) { continentesConquistados.add(continente); }
+        }
+        return continentesConquistados;
     }
 
 }
