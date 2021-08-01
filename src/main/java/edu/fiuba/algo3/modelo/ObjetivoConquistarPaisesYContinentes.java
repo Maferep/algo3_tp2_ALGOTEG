a403e78ade40f2350123d281ad7f0c3e27eeaf20
Mafer <mf.pont.tv@gmail.com>
2021-07-31 22:02:39 -0300

    implementa continentes en objetivo

--- src/main/java/edu/fiuba/algo3/modelo/ObjetivoConquistarPaisesYContinentes.java
+++ src/main/java/edu/fiuba/algo3/modelo/ObjetivoConquistarPaisesYContinentes.java
@@ -1,17 +1,19 @@
 package edu.fiuba.algo3.modelo;
+
 import edu.fiuba.algo3.modelo.Interfaces.*;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class ObjetivoConquistarPaisesYContinentes extends ObjetivoBase {
-    IJugador duenio;
     List<Continente> continentesAConquistar;
     List<IPais> paisesAConquistar;
 
     public ObjetivoConquistarPaisesYContinentes(List<Continente> continentes, List<IPais> paises) {
-        continentesAConquistar = continentes;
-        // TODO: continentes no es estado, sino que en el constructor se agregan los
-        // paises continentes
-        paisesAConquistar = paises;
+        paisesAConquistar = new ArrayList<>();
+        paisesAConquistar.addAll(paises);
+        for(Continente continente : continentes) 
+            paisesAConquistar.addAll(continente.obtenerPaises());
     }
 
     @Override
