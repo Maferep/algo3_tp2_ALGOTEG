e00b2ca177c1c69298778f331c13f582e6e70f7d
Mafer <mf.pont.tv@gmail.com>
2021-08-02 17:14:15 -0300

    imprimir mensaje si tarjetas estan vacias

--- src/main/java/edu/fiuba/algo3/vista/VistaTarjetas.java
+++ src/main/java/edu/fiuba/algo3/vista/VistaTarjetas.java
@@ -1,5 +1,7 @@
 package edu.fiuba.algo3.vista;
 
+import java.util.List;
+
 import edu.fiuba.algo3.modelo.Juego;
 import edu.fiuba.algo3.modelo.Tarjeta;
 import edu.fiuba.algo3.vista.interfases.IVista;
@@ -19,12 +21,17 @@ public class VistaTarjetas implements IVista{
     
     public void visualizar() {
         HBox tarjetasJugador = new HBox(5);
-        for(Tarjeta tarjeta : juego.jugadorActual().obtenerTarjetas()){
+        List<Tarjeta> tarjetas = juego.jugadorActual().obtenerTarjetas();
+        for(Tarjeta tarjeta : tarjetas){
             Label nombreTarjeta = new NombreJugador(tarjeta.obtenerPais().obtenerNombre());
             Label simboloTarjeta = new NombreJugador(tarjeta.obtenerSimbolo().simbolo);
             VBox contenedorTarjeta = new VBox(nombreTarjeta, simboloTarjeta);
             tarjetasJugador.getChildren().add(contenedorTarjeta);
-            
+        }
+        if(tarjetas.isEmpty()){
+
+            Label sinCartas = new Label("No tienes ninguna tarjeta de pais :(");
+            tarjetasJugador.getChildren().add(sinCartas);
         }
         contenedorJuego.definirSobreMapa(tarjetasJugador);
     }
