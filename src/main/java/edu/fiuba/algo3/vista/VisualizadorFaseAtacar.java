0aa7bb59ab7d4744e9dcec3f59aaaf1429768d69
Chiara Di Nardo <cdinardo@fi.uba.ar>
2021-08-02 21:41:36 -0300

    Visualizador de ataque implementado

--- src/main/java/edu/fiuba/algo3/vista/VisualizadorFaseAtacar.java
+++ src/main/java/edu/fiuba/algo3/vista/VisualizadorFaseAtacar.java
@@ -2,7 +2,10 @@ package edu.fiuba.algo3.vista;
 
 import edu.fiuba.algo3.modelo.Interfaces.IPais;
 import edu.fiuba.algo3.modelo.Juego;
+import edu.fiuba.algo3.modelo.fases.FaseInicio;
+import edu.fiuba.algo3.vista.eventos.BotonPaisAtacarEventHandler;
 import edu.fiuba.algo3.vista.eventos.BotonPaisColocarEventHandler;
+import edu.fiuba.algo3.vista.eventos.BotonVolver;
 import edu.fiuba.algo3.vista.eventos.PasajeDeFases;
 import edu.fiuba.algo3.vista.interfases.IVista;
 import edu.fiuba.algo3.vista.interfases.IVistaFases;
@@ -22,6 +25,8 @@ public class VisualizadorFaseAtacar implements IVista, IVistaFases {
         this.juego = juego;
         this.contenedorJuego = contenedorJuego;
         contenedor = new VBox();
+        contenedor.setSpacing(10);
+        contenedor.setPadding(new Insets(100));
     }
 
     @Override
@@ -40,12 +45,14 @@ public class VisualizadorFaseAtacar implements IVista, IVistaFases {
     private void mostrarPaises() {
         List<IPais> paisesJugador = juego.jugadorActual().obtenerPaises();
         for (IPais pais : paisesJugador) {
-            Button botonPais = new Button(pais.obtenerNombre());
+            Button botonPais = new Button();
+            botonPais.setText(pais.obtenerNombre() + " (" + pais.cantidadEjercitos() + ")");
             contenedor.getChildren().add(botonPais);
-        }
 
-        contenedor.setSpacing(10);
-        contenedor.setPadding(new Insets(100));
+            BotonPaisAtacarEventHandler evento = new BotonPaisAtacarEventHandler(pais, juego, contenedorJuego);
+
+            if (pais.cantidadEjercitos() > 1 ) { botonPais.setOnAction(evento); }
+        }
     }
 
     public void visualizarNuevaFase() {
---
+++ src/main/java/edu/fiuba/algo3/vista/eventos/BotonPaisAtacarEventHandler.java
@@ -0,0 +1,26 @@
+package edu.fiuba.algo3.vista.eventos;
+
+import edu.fiuba.algo3.modelo.Interfaces.IPais;
+import edu.fiuba.algo3.modelo.Juego;
+import edu.fiuba.algo3.vista.ContenedorJuego;
+import edu.fiuba.algo3.vista.VisualizadorAdyacentesEventHandler;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+public class BotonPaisAtacarEventHandler implements EventHandler<ActionEvent> {
+    IPais pais;
+    Juego juego;
+    ContenedorJuego contenedorJuego;
+
+    public BotonPaisAtacarEventHandler(IPais pais, Juego juego, ContenedorJuego contenedorJuego) {
+        this.pais = pais;
+        this.juego = juego;
+        this.contenedorJuego = contenedorJuego;
+    }
+
+    @Override
+    public void handle(ActionEvent actionEvent) {
+        VisualizadorAdyacentesEventHandler visualizadorAdyacentesEventHandler = new VisualizadorAdyacentesEventHandler(juego, pais, contenedorJuego);
+        visualizadorAdyacentesEventHandler.visualizar();
+    }
+}
